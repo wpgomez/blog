87f22176844e6273cccd143f4f79b388d1990f72
Willy Gomez <wp_gomez@hotmail.com>
2019-04-09 11:37:20 -0500

    seccion 1.9 integrando el login - parte 1

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        factory(App\User::class)->create([
+            'name' => 'Willy Gomez',
+            'email' => 'wp_gomez@hotmail.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+
+        factory(App\User::class, 3)->create();
+    }
+}
