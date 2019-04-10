2cd162dd17090ff71ddfde0850ab5554f7b90aa5
Willy Gomez <wp_gomez@hotmail.com>
2019-04-10 18:46:46 -0500

    seccion 3.29 mostrando las fotos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,21 @@
 @section('content')
     <section class="posts container">
         @foreach ($posts as $post)
-            <article class="post no-image">
+            <article class="post">
+                @if ($post->photos->count() === 1)
+                    <figure><img src="{{ $post->photos->first()->url }}" alt="" class="img-responsive"></figure>
+                @elseif($post->photos->count() > 1)
+                    <div class="gallery-photos" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 464 }'>
+                        @foreach ($post->photos->take(4) as $photo)
+                            <figure class="grid-item grid-item--height2">
+                                @if ($loop->iteration === 4)
+                                    <div class="overlay">{{ $post->photos->count() }} Fotos</div>
+                                @endif
+                                <img src="{{ $photo->url }}" class="img-responsive">
+                            </figure>
+                        @endforeach
+                    </div>
+                @endif
                 <div class="content-post">
                     <header class="container-flex space-between">
                         <div class="date">
