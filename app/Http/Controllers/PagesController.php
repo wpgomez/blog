804081c74cc70033a15fbfa3187d243f6285cdeb
Willy Gomez <wp_gomez@hotmail.com>
2019-04-11 11:14:54 -0500

    seccion 4.34 paginacion personalizada

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,7 @@ class PagesController extends Controller
     public function home()
     {
         $posts = Post::published()
-                ->get();
+                ->paginate(5);
 
         return view('welcome', compact('posts'));
     }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -20,7 +20,8 @@ class PostsTableSeeder extends Seeder
         $post->url = str_slug($post->title);
         $post->excerpt = "Extracto de mi primer post";
         $post->body = "<p>Contenido de mi primer post</p>";
-        $post->published_at = Carbon::now()->subDays(4);
+        $post->iframe = "<iframe width='100%' height='480' src='https://www.youtube.com/embed/1B3Zc6Be4pM' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        $post->published_at = Carbon::now()->subDays(7);
         $post->category_id = 1;
         $post->save();
 
@@ -29,7 +30,7 @@ class PostsTableSeeder extends Seeder
         $post->url = str_slug($post->title);
         $post->excerpt = "Extracto de mi segundo post";
         $post->body = "<p>Contenido de mi segundo post</p>";
-        $post->published_at = Carbon::now()->subDays(3);
+        $post->published_at = Carbon::now()->subDays(6);
         $post->category_id = 2;
         $post->save();
 
@@ -38,7 +39,7 @@ class PostsTableSeeder extends Seeder
         $post->url = str_slug($post->title);
         $post->excerpt = "Extracto de mi tercer post";
         $post->body = "<p>Contenido de mi tercer post</p>";
-        $post->published_at = Carbon::now()->subDays(2);
+        $post->published_at = Carbon::now()->subDays(5);
         $post->category_id = 1;
         $post->save();
 
@@ -47,6 +48,34 @@ class PostsTableSeeder extends Seeder
         $post->url = str_slug($post->title);
         $post->excerpt = "Extracto de mi cuarto post";
         $post->body = "<p>Contenido de mi cuarto post</p>";
+        $post->published_at = Carbon::now()->subDays(4);
+        $post->category_id = 3;
+        $post->save();
+
+        $post = new Post;
+        $post->title = "Mi quinto post";
+        $post->url = str_slug($post->title);
+        $post->excerpt = "Extracto de mi quinto post";
+        $post->body = "<p>Contenido de mi quinto post</p>";
+        $post->published_at = Carbon::now()->subDays(3);
+        $post->category_id = 3;
+        $post->save();
+
+        $post = new Post;
+        $post->title = "Mi sexto post";
+        $post->url = str_slug($post->title);
+        $post->excerpt = "Extracto de mi sexto post";
+        $post->body = "<p>Contenido de mi sexto post</p>";
+        $post->published_at = Carbon::now()->subDays(2);
+        $post->category_id = 3;
+        $post->save();
+
+        $post = new Post;
+        $post->title = "Mi septimo post";
+        $post->url = str_slug($post->title);
+        $post->excerpt = "Extracto de mi septimo post";
+        $post->body = "<p>Contenido de mi septimo post</p>";
+        $post->iframe = "<iframe width='100%' height='480' src='https://www.youtube.com/embed/1B3Zc6Be4pM' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
         $post->published_at = Carbon::now()->subDays(1);
         $post->category_id = 3;
         $post->save();
